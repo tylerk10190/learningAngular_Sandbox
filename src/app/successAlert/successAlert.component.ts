a09d4677655fbc211efbb24324eba0e0410e62c9
tylerk10190 <t.koepk10190@gmail.com>
2019-12-10 14:50:17 -0600

    just messing around with simple component creations. I added two components called successAlert and warningAlert respectively. They just simply output headers that say warning or success. I then added som simple styling in order to make them a little more interesting. I used both inline and external styling to get used to the different ways that you can reference styling with Angular. At this point, I am just getting used to how to connect and pass information via imports and exports between our main app.component and app.module, with our new components.

---
+++ src/app/successAlert/successAlert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-successAlert',
+  templateUrl: './successAlert.component.html',
+  styleUrls: ['./successAlert.component.css']
+
+})
+
+export class SuccessAlertComponent{
+
+}
---
+++ src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warningAlert',
+  templateUrl: './warningAlert.component.html',
+  styles: [`
+    h3{
+      width: 100px;
+      height: 30px;
+      color: white;
+      background-color: red;
+      text-align: center;
+    }
+    `]
+})
+
+export class WarningAlertComponent{
+
+}
