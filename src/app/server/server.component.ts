e04dfbb0d21f5a4c093c6a29a5e149eaddd0c192
tylerk10190 <t.koepk10190@gmail.com>
2019-12-15 17:01:24 -0600

    I added a button to the servers.component.html file that is disabled and then with simple property binding, I was able to bind the property allowNewServer in order to show that the the boolean controlling whether or not the button is disabled changes from false to true.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 export class ServerComponent{
 serverID: number = 10;
 serverStatus: string = 'offline';
+
+getServerStatus() {
+  return this.serverStatus;
+}
+
 }
